2250383a8390d0f2acee7668cd6a7d27ec6fffe1
MiksDev04 <mikogapasan04@gmail.com>
2025-03-28 17:54:56 +0800

    changes

--- Admin/src/script.js
+++ Admin/src/script.js
@@ -98,7 +98,7 @@ sidebar.addEventListener('click', (event) => {
 
 // Ensure the chart is initialized when the page loads
 window.addEventListener('load', () => {
-    loadPage('./Admin/src/reports.html');
+    loadPage('./Admin/src/dashboard.html');
 });
 
 function ChangeNavigationFocus(btnClick) {
